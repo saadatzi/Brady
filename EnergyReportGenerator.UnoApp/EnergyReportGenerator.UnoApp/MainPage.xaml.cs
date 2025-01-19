6f5f3c7d5178575fceecf90a532739442da72349
Saadatzi <saadatzi@gmail.com>
2025-01-19 15:30:04 +0100

    feat(FilePicker): Implement file selection using FileOpenPicker

--- EnergyReportGenerator.UnoApp/EnergyReportGenerator.UnoApp/MainPage.xaml.cs
+++ EnergyReportGenerator.UnoApp/EnergyReportGenerator.UnoApp/MainPage.xaml.cs
@@ -1,6 +1,39 @@
+using Windows.Storage.Pickers;
+using System.Xml.Serialization;
+using EnergyReportGenerator.Models;
+using EnergyReportGenerator.Services;
 
 namespace EnergyReportGenerator.UnoApp;
 
 public sealed partial class MainPage : Page
 {
+    private async Task SelectGenerationReportButton_Click(object sender, RoutedEventArgs e)
+    {
+        _generationReport = await PickFileAsync();
+        GenerationReportPathTextBlock.Text = Path.GetFileName(_generationReport.Path) ?? "No file selected";
+    }
+
+    private async Task SelectReferenceDataButton_Click(object sender, RoutedEventArgs e)
+    {
+        _referenceData = await PickFileAsync();
+        ReferenceDataPathTextBlock.Text = Path.GetFileName(_referenceData.Path) ?? "No file selected";
+    }
+
+    private async Task<StorageFile> PickFileAsync()
+    {
+        var openPicker = new FileOpenPicker();
+        openPicker.ViewMode = PickerViewMode.Thumbnail;
+        openPicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+        openPicker.FileTypeFilter.Add(".xml");
+
+        var window = Window.Current;
+
+        var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+
+        WinRT.Interop.InitializeWithWindow.Initialize(openPicker, hWnd);
+
+        StorageFile file = await openPicker.PickSingleFileAsync();
+        return file;
+    }
+
 }
