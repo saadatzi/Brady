974d133853491f95efdcf795436a45be36601557
Saadatzi <saadatzi@gmail.com>
2025-01-20 12:14:37 +0100

    feat(FileProcessorService): Instrument FileProcessorService with logging and tracing

--- EnergyReportGenerator/Services/FileProcessorService.cs
+++ EnergyReportGenerator/Services/FileProcessorService.cs
@@ -12,18 +12,21 @@ public class FileProcessorService : IFileProcessorService
     private readonly IConfiguration _configuration;
     private readonly IXmlService _xmlService;
     private readonly IGenerationCalculatorService _calculatorService;
+    private readonly ActivitySource _activitySource;
     private FileSystemWatcher? _watcher;
 
     public FileProcessorService(
         ILogger<FileProcessorService> logger,
         IConfiguration configuration,
         IXmlService xmlService,
-        IGenerationCalculatorService calculatorService)
+        IGenerationCalculatorService calculatorService,
+        ActivitySource activitySource)
     {
         _logger = logger;
         _configuration = configuration;
         _xmlService = xmlService;
         _calculatorService = calculatorService;
+        _activitySource = activitySource;
 
         Directory.CreateDirectory(_configuration["InputFolder"]!);
         Directory.CreateDirectory(_configuration["OutputFolder"]!);
@@ -32,6 +35,8 @@ public class FileProcessorService : IFileProcessorService
 
     public async Task StartAsync(CancellationToken cancellationToken)
     {
+        using var activity = _activitySource.StartActivity($"{nameof(FileProcessorService)}.{nameof(StartAsync)}");
+
         var inputFolder = _configuration["InputFolder"]!;
 
         _watcher = new FileSystemWatcher(inputFolder!, "*.xml");
@@ -40,10 +45,12 @@ public class FileProcessorService : IFileProcessorService
 
         await ProcessExistingFilesAsync(inputFolder);
         _logger.LogInformation($"Watching for XML files in: {inputFolder}");
+        activity?.AddEvent(new ActivityEvent("Started watching for files."));
     }
 
     public async Task MultiprocessingStartAsync(CancellationToken cancellationToken)
     {
+        using var activity = _activitySource.StartActivity($"{nameof(FileProcessorService)}.{nameof(MultiprocessingStartAsync)}");
         var inputFolder = _configuration["InputFolder"]!;
 
         _watcher = new FileSystemWatcher(inputFolder, "*.xml");
@@ -52,11 +59,14 @@ public class FileProcessorService : IFileProcessorService
 
         await ProcessExistingFilesParallelAsync(inputFolder, cancellationToken);
         _logger.LogInformation($"Watching for XML files in: {inputFolder} with multiprocessing");
+        activity?.AddEvent(new ActivityEvent("Started watching for files with multiprocessing."));
     }
 
     private async Task ProcessExistingFilesParallelAsync(string inputFolder, CancellationToken cancellationToken)
     {
+        using var activity = _activitySource.StartActivity($"{nameof(FileProcessorService)}.{nameof(ProcessExistingFilesParallelAsync)}");
         _logger.LogInformation($"Processing existing XML files in: {inputFolder}");
+        activity?.AddEvent(new ActivityEvent("Started processing existing files in parallel."));
 
         var options = new ParallelOptions
         {
@@ -73,69 +83,141 @@ public class FileProcessorService : IFileProcessorService
             catch (Exception ex)
             {
                 _logger.LogError(ex, $"Error processing existing file: {filePath}");
+                activity?.RecordException(ex);
             }
         });
 
         _logger.LogInformation($"Finished processing all existing files.");
+        activity?.AddEvent(new ActivityEvent("Finished processing existing files in parallel."));
     }
         
     public void Stop(CancellationToken cancellationToken)
     {
+        using var activity = _activitySource.StartActivity($"{nameof(FileProcessorService)}.{nameof(Stop)}");
         if(_watcher != null)
         {
             _watcher!.EnableRaisingEvents = false;
             _watcher.Dispose();
             _logger.LogInformation("Stopped watching for XML files.");
+            activity?.AddEvent(new ActivityEvent("Stopped watching for files."));
         }
 
         _logger.LogInformation("FileProcessorService stopped");
+        activity?.AddEvent(new ActivityEvent("FileProcessorService stopped."));
     }
 
 
     private async Task ProcessExistingFilesAsync(string inputFolder)
     {
+        using var activity = _activitySource.StartActivity($"{nameof(FileProcessorService)}.{nameof(ProcessExistingFilesAsync)}");
         _logger.LogInformation($"Processing existing XML files in: {inputFolder}");
+        activity?.AddEvent(new ActivityEvent("Started processing existing files."));
 
         var tasks = Directory.GetFiles(inputFolder, "*.xml").Select(ProcessFileAsync);
 
         await Task.WhenAll(tasks);
 
         _logger.LogInformation($"Finished processing all existing files.");
+        activity?.AddEvent(new ActivityEvent("Finished processing existing files."));
     }
 
     private async void OnFileCreated(object sender, FileSystemEventArgs e)
     {
-        _logger.LogInformation($"New file created: {e.FullPath}");
-        await ProcessFileAsync(e.FullPath);
+        using (LogContext.PushProperty("FileName", Path.GetFileName(e.FullPath)))
+        {
+            using var activity = _activitySource.StartActivity($"{nameof(FileProcessorService)}.{nameof(OnFileCreated)}");
+            _logger.LogInformation("New file created: {FullPath}", e.FullPath);
+            activity?.SetTag("file.path", e.FullPath);
+            await ProcessFileAsync(e.FullPath);
+        }
     }
 
     private async Task ProcessFileAsync(string filePath)
     {
-        try
+        using (LogContext.PushProperty("FileName", Path.GetFileName(filePath)))
         {
-            // Deserialize the GenerationReport XML
-            var generationReport = await _xmlService.DeserializeGenerationReportAsync(filePath);
+            using var activity = _activitySource.StartActivity($"{nameof(FileProcessorService)}.{nameof(ProcessFileAsync)}");
+            activity?.SetTag("file.path", filePath);
+            _logger.LogInformation("Processing file: {FilePath}", filePath);
+            activity?.AddEvent(new ActivityEvent("Started processing file."));
+        
 
-            // Load reference data
-            var referenceData = await _xmlService.DeserializeReferenceDataAsync(_configuration["ReferenceDataPath"]!);
+            try
+            {
+                // Deserialize the GenerationReport XML
+                var generationReport = new GenerationReport();
+                using (var deserializeActivity = _activitySource.StartActivity($"{nameof(IXmlService)}.{nameof(_xmlService.DeserializeGenerationReportAsync)}"))
+                {
+                    deserializeActivity?.SetTag("file.path", filePath);
+                    generationReport = await _xmlService.DeserializeGenerationReportAsync(filePath);
+                    deserializeActivity?.SetTag("success", generationReport != null);
+                    if (generationReport == null)
+                    {
+                        deserializeActivity?.SetStatus(ActivityStatusCode.Error, "Failed to deserialize GenerationReport.");
+                        return;
+                    }
+                }
 
-            // Calculate the required values
-            var generationOutput = _calculatorService.Calculate(generationReport, referenceData);
+                // Load reference data
+                var referenceData = new ReferenceData();
+                using (var referenceDataActivity = _activitySource.StartActivity($"{nameof(IXmlService)}.{nameof(_xmlService.DeserializeReferenceDataAsync)}"))
+                {
+                    var referenceDataPath = _configuration["ReferenceDataPath"]!;
+                    referenceDataActivity?.SetTag("file.path", referenceDataPath);
+                    referenceData = await _xmlService.DeserializeReferenceDataAsync(referenceDataPath);
+                    referenceDataActivity?.SetTag("success", referenceData != null);
+                    if (referenceData == null)
+                    {
+                        referenceDataActivity?.SetStatus(ActivityStatusCode.Error, "Failed to deserialize ReferenceData.");
+                        return;
+                    }
+                }
 
-            // Serialize the GenerationOutput to XML
-            var inputFilename = Path.GetFileNameWithoutExtension(filePath);
-            var outputFilePath = Path.Combine(_configuration["OutputFolder"]!, $"{inputFilename}_GenerationOutput_{DateTime.Now:yyyyMMddHHmmss}.xml");
-            await _xmlService.SerializeGenerationOutputAsync(generationOutput, outputFilePath);
+                // Calculate the required values
+                var generationOutput = new GenerationOutput();
+                using (var calculateActivity = _activitySource.StartActivity($"{nameof(IGenerationCalculatorService)}.{nameof(_calculatorService.Calculate)}"))
+                {
+                    generationOutput = _calculatorService.Calculate(generationReport, referenceData);
+                }
 
-            _logger.LogInformation($"Processed and saved output to: {outputFilePath}");
+                // Serialize the GenerationOutput to XML
+                string outputFilePath;
+                using (var serializeActivity = _activitySource.StartActivity($"{nameof(IXmlService)}.{nameof(_xmlService.SerializeGenerationOutputAsync)}"))
+                {
+                    var inputFilename = Path.GetFileNameWithoutExtension(filePath);
+                    outputFilePath = Path.Combine(_configuration["OutputFolder"]!, $"{inputFilename}_GenerationOutput_{DateTime.Now:yyyyMMddHHmmss}.xml");
+                    serializeActivity?.SetTag("file.path", outputFilePath);
+                    await _xmlService.SerializeGenerationOutputAsync(generationOutput, outputFilePath);
+                }
 
-            File.Move(filePath, Path.Combine(_configuration["Processed"]!, inputFilename + ".xml") );
+                _logger.LogInformation("Processed and saved output to: {OutputFilePath}", outputFilePath);
+                activity?.AddEvent(new ActivityEvent(
+                    "Processed file and saved output.",
+                    tags:new ActivityTagsCollection { { "outputFilePath", outputFilePath } }
+                ));
 
-            _logger.LogInformation($"Processed file moved to Processed Folder : {inputFilename}");
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, $"Error processing file: {filePath}");
+                string processedFilePath;
+                using (var moveActivity = _activitySource.StartActivity("MoveProcessedFile"))
+                {
+                    var inputFilename = Path.GetFileNameWithoutExtension(filePath);
+                    processedFilePath = Path.Combine(_configuration["Processed"]!, inputFilename + ".xml");
+                    moveActivity?.SetTag("source.path", filePath);
+                    moveActivity?.SetTag("destination.path", processedFilePath);
+                    File.Move(filePath, processedFilePath);
+                }
+
+                _logger.LogInformation("Processed file moved to Processed Folder : {ProcessedFilePath}", Path.GetFileName(processedFilePath));
+                activity?.AddEvent(new ActivityEvent(
+                    "Moved processed file.",
+                    tags: new ActivityTagsCollection { { "processedFilePath", processedFilePath } }
+                ));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error processing file: {FilePath}", filePath);
+                activity?.SetStatus(ActivityStatusCode.Error, ex.Message);
+                activity?.RecordException(ex);
+            }
         }
     }
 }
